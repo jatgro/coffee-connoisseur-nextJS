d6aa60d9ae37937edef4476298be416a5bf9d961
Jatin grover <akazasama@Jatins-MacBook-Air.local>
2022-12-10 14:18:21 +0530

    adding background and minor cleaningh

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2022 learning Next js </p>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
-
-
+export default MyApp;
